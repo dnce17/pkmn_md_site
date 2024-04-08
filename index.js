beeb6908d952f6d09bd406b4facc7e2111837690
Danny Chen <dannychen@Dannys-Air.lan1>
2024-04-08 19:12:58 -0400

    feat: toggleGamesSelection() func complete

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const navToggle = document.querySelector(".nav__toggle");
-const nav = document.querySelector(".nav");
+function changeText (variable, desiredText) {
+    variable.textContent = desiredText;
+}
 
-navToggle.addEventListener("click", () => {
-    nav.classList.toggle("visible")
-})
+function toggleNav() {
+    const navToggle = document.querySelector(".nav__toggle");
+    const nav = document.querySelector(".nav");
+
+    navToggle.addEventListener("click", () => {
+        nav.classList.toggle("visible");
+    })
+}
+toggleNav();
+
+function toggleGamesSelection() {
+    const btn = document.querySelector(".games__released-btn");
+    const selections = document.querySelectorAll(".games__released-selection");
+    
+    btn.addEventListener("click", () => {
+        for (let i = 1; i < selections.length; i++) {
+            selections[i].classList.toggle("visible");
+            if (btn.textContent.toLowerCase() == "show more") {
+                changeText(btn, "Hide");
+            } 
+            else {
+                changeText(btn, "Show More");
+            }
+        }
+    })
+}
+toggleGamesSelection();
